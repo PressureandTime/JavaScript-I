e2247db0c027df30055d843d78ab7d3a808f8924
Petar Scepanovic <hiborija@gmail.com>
2019-05-07 14:29:52 +0200

    finished objects, started stretch challenge, coming back to it later

--- assignments/objects.js
+++ assignments/objects.js
@@ -31,7 +31,10 @@ const kennanObject = {
   id: 2,
   name: "Kennan",
   email:"kdiben1@tinypic.com",
-  gender: "M" 
+  gender: "M",
+  speak: function() {
+    return "Hello my name is Kennan"
+  } 
 }
 
 const kevenObject = {
@@ -53,7 +56,10 @@ const antoniettaObject  = {
   id: 5,
   name: "Antonietta",
   email:"adaine5@samsung.com",
-  gender: "F" 
+  gender: "F",
+  multiplyNums: function(param1,param2){
+    return param1*param2
+  }
 }
 
 
@@ -80,11 +86,12 @@ console.log(antoniettaObject.gender);
 // ==== Challenge 3: Object Methods ==== 
 // Give Kennan the ability to say "Hello, my name is Kennan!" Use the console.log provided as a hint.
 // console.log(kennan.speak());
-kennanObject.function speak() {
-  
-}
+console.log(kennanObject.speak());
+
 // Antonietta loves math, give her the ability to multiply two numbers together and return the product. Use the console.log provided as a hint.
 //console.log(antonietta.multiplyNums(3,4));
+
+console.log(antoniettaObject.multiplyNums(3,4));
 
 // === Great work! === Head over to the the arrays.js file or take a look at the stretch challenge
 
@@ -95,7 +102,18 @@ kennanObject.function speak() {
 // 3. Nest a grandchild object in the child object with properties for name and age.  The name will be Sam and the age will be 30
 // 4. Give each of the objects the ability to speak their names using the this keyword.
 
-const parent = {}
+const parent = {
+  name:"Susan",
+  age:70
+  childObject : {
+    name: "George",
+    age:50
+    grandChildObject:{
+      name: "Sam",
+      age:30
+    }
+  }
+}
 
 // Log the parent object's name
 
